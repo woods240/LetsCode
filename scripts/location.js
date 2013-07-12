5bfa55f699ada867ec5fa4ef809f046cff8e755b
woods240 <woods240@gmail.com>
2013-07-12 14:08:38 +0800

    add anchor response in browser
    can spedify page by url contains anchor

--- scripts/location.js
+++ scripts/location.js
@@ -3,13 +3,12 @@
     /* Location CLASS DEFINITION
     * ========================= */
     var Location = function ( options ) {
-        this.$mainMenu = $( '#' + options.mainMenuId );
-        this.$subMenu = $( '#' + options.subMenuId );
-        this.$breadcrumb = $( '#' + options.breadcrumbId );
-        this.$content = $( '#' + options.contentId );
+        this.$NavMenu = $( '#' + options.navMenuId );
+        this.$SubMenu = $( '#' + options.subMenuId );
+        this.$Breadcrumb = $( '#' + options.breadcrumbId );
+        this.$Content = $( '#' + options.contentId );
 
-        this.$location = null;
-        this.getLocationFromSiteMap( options.siteMapPath );
+        this.$Location = this.getLocationFromSiteMap( options.siteMapPath );
 
         this.onContentLoad = options.onContentLoad;
         this.onContentLoading = options.onContentLoading;
@@ -20,12 +19,13 @@
         constructor: Location
 
       , getLocationFromSiteMap: function ( siteMapPath ) {
-          var that = this;
+          var $siteMap;
+
           $.ajax( {
               url: siteMapPath,
               dataType: 'xml',
               success: function ( xml ) {
-                  that.$location = $( 'location', xml );
+                  $siteMap = $( 'location', xml );
               },
               error: function ( XMLHttpRequest, textStatus, errorThrown ) {
                   alert( errorThrown );
@@ -34,118 +34,177 @@
               type: 'GET',
               async: false
           } );
+
+          return $siteMap;
       }
 
-      , createMenuItem: function ( text, url ) {
-          return '<li><a href="' + ( url || '#' ) + '">' + text + '</a></li>';
+      , get$Nav: function ( navText ) {
+          return this.$Location.find( 'nav[text="' + navText + '"]' );
       }
 
-      , createCetegoryItem: function ( text ) {
-          return '<li class="nav-header">' + text + '</li>';
+      , get$Category: function ( categoryText ) {
+          return this.$Location.find( 'category[text="' + categoryText + '"]' );
       }
 
-      , createBreadcrumbItem: function ( text, isActive ) {
-          if ( isActive ) {
-              return '<li class="active">' + text + '</li>';
+      , get$Item: function ( anchor ) {
+          var $Item = this.$Location.find( 'item[name="' + anchor + '"]' );
+
+          // 如果指定的 anchor 不存在，就使用默认的（第一个anchor）
+          if ( $Item.length == 0 ) {
+              $Item = this.$Location.find( 'item[name]' ).first();
           }
 
-          return '<li><a href="#">' + text + '</a><span class="divider">/</span></li>';
+          return $Item;
       }
 
-      , activeItem: function ( $linkItem ) {
-          $linkItem.parent().addClass( 'active' ).siblings().removeClass( 'active' );
-      }
 
-      , refreshNav: function () {
-          var menuItems = ''
-            , that = this;
+      , loadNav: function () {
+          var that = this;
 
-          this.$location.find( 'nav' ).each( function () {      // 刷新nav
-              var navText = $( this ).attr( 'text' );
-              menuItems += that.createMenuItem( navText );
+          that.$NavMenu.empty();
+          this.$Location.find( 'nav[text]:has(item[name])' ).each( function () {
+              var $Nav = $( this )
+                , $NavLink = that.createNavLink( $Nav );
+
+              that.$NavMenu.append( $NavLink );
           } );
-          this.$mainMenu.html( menuItems );
-
-          this.$mainMenu.find( 'a' ).on( 'click', function ( event ) {     // 绑定点击事件
-              var $linkItem = $( this )
-                , navText = $linkItem.text();
-
-              that.activeItem( $linkItem );
-              that.refreshSubMenu( navText );
-
-              event.preventDefault();
-          } ).first().trigger( 'click' );
       }
 
-      , refreshSubMenu: function ( navText ) {
-          var subMenuItems = ''
-            , that = this;
+      , loadSubMenu: function ( navText ) {
+          var that = this;
 
-          this.$location.find( 'nav[text=' + navText + ']' ).find( 'category' ).each( function () {    // 刷新subMenu
-              var $categoryItem = $( this )
-                , categoryText = $categoryItem.attr( 'text' );
+          that.$SubMenu.empty();
+          this.get$Nav( navText ).find( 'category[text]' ).each( function () {
+              var $Category = $( this )
+                , $CategoryText = that.createCategoryText( $Category );
 
-              subMenuItems += that.createCetegoryItem( categoryText );
-              $categoryItem.find( 'item' ).each( function () {
-                  var linkText = $( this ).text()
-                    , linkUrl = $( this ).attr( 'url' );
-                  subMenuItems += that.createMenuItem( linkText, linkUrl );
+              that.$SubMenu.append( $CategoryText );
+
+              $Category.find( 'item[name]' ).each( function () {
+                  var $Item = $( this )
+                    , $ItemLink = that.createItemLink( $Item );
+
+                  that.$SubMenu.append( $ItemLink );
               } );
           } );
-          this.$subMenu.html( subMenuItems );
-
-          this.$subMenu.find( 'a' ).on( 'click', function ( event ) {     // 绑定点击事件
-              var $linkItem = $( this )
-                , linkUrl = $linkItem.attr( 'href' );
-
-              that.activeItem( $linkItem );
-              that.refreshBreadcrumb( $linkItem.text() );
-              that.refreshContent( linkUrl );
-
-              event.preventDefault();
-          } ).first().trigger( 'click' );
       }
 
-      , refreshBreadcrumb: function ( itemText ) {
-          var $linkItem = this.$location.find( 'item:contains("' + itemText + '")' ).first()
-            , $categoryItem = $linkItem.parent().first()
-            , $navItem = $categoryItem.parent().first()
-            , breadcrumbItems = '';
+      , loadBreadcrumb: function ( $Item ) {
+          var $Category = $Item.parent().first()
+            , $Nav = $Category.parent().first();
 
-          breadcrumbItems += this.createBreadcrumbItem( $navItem.attr( 'text' ) );
-          breadcrumbItems += this.createBreadcrumbItem( $categoryItem.attr( 'text' ) );
-          breadcrumbItems += this.createBreadcrumbItem( $linkItem.text(), true );
+          var $ItemText = $( '<li class="active">' + $Item.text() + '</li>' )
+            , $CategoryLink = this.createCategoryLink( $Category ).append( '<span class="divider">/</span>' )
+            , $NavLink = this.createCategoryLink( $Nav ).append( '<span class="divider">/</span>' );
 
-          this.$breadcrumb.html( breadcrumbItems );
+          this.$Breadcrumb.empty();
+          this.$Breadcrumb.append( $NavLink ).append( $CategoryLink ).append( $ItemText );
       }
 
-      , refreshContent: function ( url ) {
-          if ( url != '#' ) {
-              var that = this;
-              that.onContentLoading();
-              $.ajax( {
-                  url: url,
-                  dataType: 'html',
-                  success: function ( data, textStatus, jqXHR ) {
-                      that.$content.html( data );
-                      that.onContentUpdate();
-                  },
-                  complete: function ( XMLHttpRequest, textStatus ) {
-                      that.onContentLoad();
-                  },
-                  error: function ( XMLHttpRequest, textStatus, errorThrown ) {
-                      alert( errorThrown );
-                      that.$content.html( '出错啦' );
-                  },
-                  cache: false,
-                  type: 'get',
-                  async: true
+      , loadContent: function ( $Item ) {
+          var url = $Item.attr( 'url' )
+            , that = this;
+
+          if ( !url || url == '#' ) {
+              that.$Content.html( '<p>页面正在开发中，敬请期待。。。</p>' );
+              return;
+          }
+
+          that.onContentLoading();
+
+          $.ajax( {
+              url: url,
+              dataType: 'html',
+              success: function ( data, textStatus, jqXHR ) {
+                  that.$Content.html( data );
+                  that.onContentUpdate();
+              },
+              complete: function ( XMLHttpRequest, textStatus ) {
+                  that.onContentLoad();
+              },
+              error: function ( XMLHttpRequest, textStatus, errorThrown ) {
+                  alert( errorThrown );
+                  that.$Content.html( '出错啦' );
+              },
+              cache: false,
+              type: 'get',
+              async: true
+          } );
+      }
+
+      , loadPage: function ( anchor ) {
+          var $Item = this.get$Item( anchor )
+            , $Category = $Item.parent().first()
+            , $Nav = $Category.parent().first()
+            , navText = $Nav.attr( 'text' );
+
+          // 从 SiteMap 中找到 anchor, 纠正错误（如果指定的anchor不存在）
+          anchor = $Item.attr( 'name' );
+          window.location.hash = anchor;
+
+          // 加载 anchor 对应的 Nav, SubMenu, Breadcrumb, Content
+          this.loadNav();
+          this.loadSubMenu( navText );
+          this.loadBreadcrumb( $Item );
+          this.loadContent( $Item );
+
+          var $NavLink = this.$NavMenu.find( 'li:contains("' + $Nav.attr( 'text' ) + '")' )
+            , $ItemLink = this.$SubMenu.find( 'li:has(a[href="' + anchor + '"])' );
+
+          // 给 Nav 和 Item, 增加 "active" 样式
+          this.activeLink( $NavLink );
+          this.activeLink( $ItemLink );
+      }
+
+
+      , createLinkHtml: function ( anchor, text ) {
+          return '<li><a href="' + ( anchor || '#' ) + '">' + text + '</a></li>';
+      }
+
+      , createNavLink: function ( $Nav ) {
+          var anchor = $Nav.find( 'item[name]' ).first().attr( 'name' )
+            , navText = $Nav.attr( 'text' )
+            , that = this;
+
+          return $( this.createLinkHtml( anchor, navText ) )
+              .on( 'click', function ( event ) {
+                  that.activeLink( $( this ) );
+                  that.loadSubMenu( navText );
+                  that.$SubMenu.find( 'li:has(a)' ).first().trigger( 'click' );
               } );
-          }
-          else {
-              this.$content.html( '<p>页面正在开发中，敬请期待。。。</p>' );
-          }
       }
+
+      , createCategoryLink: function ( $Category ) {
+          var anchor = $Category.find( 'item[name]' ).first().attr( 'name' )
+            , categoryText = $Category.attr( 'text' );
+
+          return $( this.createLinkHtml( anchor, categoryText ) );
+      }
+
+      , createCategoryText: function ( $Category ) {
+          var categoryText = $Category.attr( 'text' );
+
+          return $( '<li class="nav-header">' + categoryText + '</li>' );
+      }
+
+      , createItemLink: function ( $Item ) {
+          var anchor = $Item.attr( 'name' )
+            , itemText = $Item.text()
+            , that = this;
+
+          return $( this.createLinkHtml( anchor, itemText ) )
+              .on( 'click', function ( event ) {
+                  that.activeLink( $( this ) );
+                  that.loadBreadcrumb( $Item );
+                  that.loadContent( $Item );
+                  //event.preventDefault();
+              } );
+      }
+
+      , activeLink: function ( $Link ) {
+          $Link.addClass( 'active' ).siblings().removeClass( 'active' );
+      }
+
     };
 
 
@@ -158,16 +217,19 @@
               , data = $this.data( 'location' );
 
             if ( !data ) $this.data( 'location', data = new Location( options ) );
-            data.refreshNav();
+
+            var anchor = window.location.hash || options.defaultAnchor;
+            data.loadPage( anchor );
         } );
     };
 
     $.fn.location.defaults = {
-        mainMenuId: 'mainMenu',
+        navMenuId: 'mainMenu',
         subMenuId: 'subMenu',
         breadcrumbId: 'breadcrumb',
         contentId: 'content',
         siteMapPath: 'content/siteMap.xml',
+        defaultAnchor: '#GitHub',
         onContentLoading: function () { },
         onContentLoad: function () { },
         onContentUpdate: function () { }
@@ -183,7 +245,6 @@
             onContentLoading: function () {
 
                 $.waitingPrompt.show( '#content', '正在加载，请稍等 ...' );   // 打开等待提示（最先执行）
-
             }
 
             /* 内容加载完成后执行 */
