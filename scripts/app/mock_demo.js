776274290911322c632ac96368e9c2857fa3399b
woods240 <woods240@gmail.com>
2014-01-02 22:23:29 +0800

    name space

---
+++ scripts/app/mock_demo.js
@@ -0,0 +1,21 @@
+﻿// 定义Rectangle类
+function Rectangle( w, h ) {
+    this.width = w;
+    this.height = h;
+}
+Rectangle.prototype.area = function () {
+    return this.width * this.height;
+}
+
+// 定义PositionedRectangle类，继承自Rectangle类
+function PositionedRectangle( x, y, w, h ) {
+    Rectangle.call( this, w, h );
+    this.x = x;
+    this.y = y;
+}
+PositionedRectangle.prototype = new Rectangle();
+PositionedRectangle.prototype.constructor = PositionedRectangle;
+PositionedRectangle.prototype.contains = function ( x, y ) {
+    return ( x > this.x && x < this.x + this.width )
+        && ( y > this.y && y < this.y + this.height );
+}
